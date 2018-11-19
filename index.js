982322c460ab60ef5203894ce7d38e0b15815360
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-19 23:51:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ function justInvoke(fn){
 }
 
 function setThisWithCall(fn, thisValue, arg){
-  return fn(thisValue, arg)
+  return fn.call(thisValue, arg)
 }
+
+function setThisWithApply(fn, thisValue, arg)
